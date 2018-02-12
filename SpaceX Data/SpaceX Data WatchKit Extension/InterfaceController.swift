f9d1dc930c113334567b7b4379e72c2b8bdf5b4d
Vadim Rupets <vadik_rupets@icloud.com>
2018-02-12 10:17:13 +0300

    Created Xcode project

---
+++ SpaceX Data/SpaceX Data WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SpaceX Data WatchKit Extension
+//
+//  Created by Vadim Rupets on 2/12/18.
+//  Copyright © 2018 Vadim Rupets. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
